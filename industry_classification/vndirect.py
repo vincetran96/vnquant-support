979911b702e708c13942266823469422ab7d8023
Vince Tran <vietanhtran.96@gmail.com>
2022-05-07 21:58:29 -0500

    Refactor code according to Pylint - Add docstring for industry class - vndirect - Re-order imports - Change mutable arguments for get_ind_class function

--- industry_classification/vndirect.py
+++ industry_classification/vndirect.py
@@ -1,4 +1,4 @@
-# Based on the industry classification API from VNDirect
+'''Based on the industry classification API from VNDirect'''
 # Basic query param structure:
 #   :params:
 #       q: the core part of the query; possible inputs:
@@ -18,12 +18,12 @@
 # TODO: How to deal with a Vietnamese name query?
 
 import random
-import httpx
 import logging
 import urllib.parse
-import pandas
 from copy import deepcopy
 from typing import List, Tuple, Literal
+import httpx
+import pandas
 import vnquant.DataLoader as vnd_loader
 from common.configs import USER_AGENTS
 
@@ -50,9 +50,9 @@ PAYLOAD_SAFE_CHARS = ":," # Not to encode these in query param
 DEFAULT_INDUSTRY_LEVEL = 1 # Default industry level
 
 def get_ind_class(
-        code_list: List[str]=[],
-        industry_codes: List[str]=[],
-        higher_level_codes: List[str]=[],
+        code_list: List[str]=None,
+        industry_codes: List[str]=None,
+        higher_level_codes: List[str]=None,
         english_name: str="",
         vietnamese_name: str="",
         result_size: int=MAX_QUERY_SIZE
@@ -72,7 +72,7 @@ def get_ind_class(
         - DataFrame: pandas DataFrame of industry classification
         - Metadata: metadata dictionary about the request
     '''
-    
+
     # Prepare payload
     # Construct a single string containing all keys for the 'q' parameter
 
@@ -84,9 +84,12 @@ def get_ind_class(
     payload = deepcopy(BASE_PAYLOAD)
     payload_q_keys = deepcopy(PAYLOAD_Q_KEYS)
     payload_q_keys['industryLevel'] = DEFAULT_INDUSTRY_LEVEL
-    payload_q_keys['codeList'] = ",".join([code for code in code_list])
-    payload_q_keys['industryCode'] = ",".join([ic for ic in industry_codes])
-    payload_q_keys['higherLevelCode'] = ",".join([hlc for hlc in higher_level_codes])
+    if code_list:
+        payload_q_keys['codeList'] = ",".join(code_list)
+    if industry_codes:
+        payload_q_keys['industryCode'] = ",".join(industry_codes)
+    if higher_level_codes:
+        payload_q_keys['higherLevelCode'] = ",".join(higher_level_codes)
     payload_q_keys['englishName'] = english_name
     payload_q_keys['englishName'] = vietnamese_name
     payload_q_str = PAYLOAD_Q_JOIN_CHAR.join(
@@ -94,8 +97,8 @@ def get_ind_class(
     )
     payload['q'] = payload_q_str
     payload['size'] = result_size
-    
-    payload_str = urllib.parse.urlencode(payload, safe=PAYLOAD_SAFE_CHARS)    
+
+    payload_str = urllib.parse.urlencode(payload, safe=PAYLOAD_SAFE_CHARS)
     headers = {
         'content-type': CONTENT_TYPE,
         'User-Agent': random.choice(USER_AGENTS)
@@ -107,10 +110,10 @@ def get_ind_class(
     )
 
     resp_json = resp.json()
-    ind_df = pandas.DataFrame(resp_json['data'])
+    industry_df = pandas.DataFrame(resp_json['data'])
     metadata_dict = {key: value for key, value in resp_json.items() if key != 'data'}
 
-    return ind_df, metadata_dict
+    return industry_df, metadata_dict
 
 
 def get_full_ind_class():
@@ -120,7 +123,7 @@ def get_full_ind_class():
     return get_ind_class()
 
 def get_price_from_ind_df(
-        ind_df: pandas.DataFrame,
+        industry_df: pandas.DataFrame,
         start: str,
         end: str,
         minimal: bool=True,
@@ -130,7 +133,7 @@ def get_price_from_ind_df(
     a industry classification DataFrame
 
     :params:
-        @ind_df: a pandas DataFrame returned from the get_ind_class function;
+        @industry_df: a pandas DataFrame returned from the get_ind_class function;
             it must at least have a `codeList` column where
             its value is a string of stock codes (e.g., "AAA,HCM")
         @start: str - start date of the period to get price for;
@@ -144,10 +147,10 @@ def get_price_from_ind_df(
             options are vnd (for VNDirect) or cafe (for CafeF)
     '''
 
-    # Get a list of strings of stock codes from the 
+    # Get a list of strings of stock codes from the
     #   `codeList` column of the provided DataFrame
     # Then pass that list into the DataLoader object
-    code_list = ",".join(list(ind_df['codeList'])).split(",")
+    code_list = ",".join(list(industry_df['codeList'])).split(",")
     loader = vnd_loader.DataLoader(
         symbols=code_list,
         start=start,
@@ -163,10 +166,10 @@ def get_price_from_ind_df(
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
-    start = '2020-02-02'
-    end = '2020-04-02'
+    START = '2020-02-02'
+    END = '2020-04-02'
     ind_df, meta = get_ind_class(code_list=["ASM", "AAA"])
-    price_df = get_price_from_ind_df(ind_df, start, end)
+    p_df = get_price_from_ind_df(ind_df, START, END)
 
     logging.info(ind_df)
-    logging.info(price_df)
+    logging.info(p_df)
